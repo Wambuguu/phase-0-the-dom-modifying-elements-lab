45256025cebd15d2aac03725f4536adf1f948049
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-03-14 14:01:39 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const element = document.getElementById("main")
+main.remove()
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+newHeader.id = 'victory'
+const yourName = 'Lewis'
+newHeader.textContent = `${yourName} is the champion`;
+
+
+
+
+
